8fed451a7fd5c719893b53abe11c2f961dbf3590
martilidad <martilidad@users.noreply.github.com>
2022-04-16 23:02:42 +0200

    initial angular project

---
+++ angular/whereami/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { IndexComponent } from './index/index.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
