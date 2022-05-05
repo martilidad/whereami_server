6c15dc1018537073978af0aba721d543a512a4fe
martilidad <martilidad@users.noreply.github.com>
2022-05-05 10:43:29 +0200

    add bootstrap dependency

--- angular/whereami/src/app/app.module.ts
+++ angular/whereami/src/app/app.module.ts
@@ -13,6 +13,7 @@ import {HttpErrorHandler} from "./http-error-handler.service";
 import {MessageService} from "./message.service";
 import {FormsModule} from "@angular/forms";
 import {ApiInterceptor} from "./api-interceptor";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +27,8 @@ import {ApiInterceptor} from "./api-interceptor";
     HttpClientXsrfModule,
     AppRoutingModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [
     HttpErrorHandler,
