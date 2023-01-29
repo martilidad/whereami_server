a350e73db65ffcde06cdba549c893ca1a1393197
martilidad <martilidad@users.noreply.github.com>
2023-01-29 11:44:05 +0100

    update to angular 15

--- angular/whereami/src/app/test/api-interceptor.ts
+++ angular/whereami/src/app/test/api-interceptor.ts
@@ -9,12 +9,29 @@ import { BACKEND_HOST } from "src/environments/environment";
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    //interceptor for debugging against django
-    const apiReq = req.clone({ url: `${window.location.protocol}//${this.hostName}${req.url}` });
+    let apiReq = req;
+    if(this.shouldIntercept(req)) {
+      //interceptor for debugging against django
+      apiReq = req.clone({ url: `${window.location.protocol}//${this.hostName}${req.url}` });
+    }
     return next.handle(apiReq);
   }
 
   private get hostName() {
     return BACKEND_HOST ? BACKEND_HOST : window.location.hostname;
+  }
+
+  private parseUrl(req: HttpRequest<any>): URL {
+    try {
+      return new URL(req.url);
+    } catch (TypeError) {
+      return new URL(req.url, window.location.href);
+    }
+
+  }
+  
+  //only intercept requests that go to our Backend
+  private shouldIntercept(req: HttpRequest<any>): boolean {
+    return this.parseUrl(req).host == window.location.host;
   }
 }
