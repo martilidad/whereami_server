374e998ca5a5f9d46ff060bb46f7f3c5bd779258
Stefan Schiechel <stefanschiechel@gmx.de>
2023-04-11 15:48:35 +0200

    fixed test

--- angular/whereami/src/app/service/file/file.service.spec.ts
+++ angular/whereami/src/app/service/file/file.service.spec.ts
@@ -1,12 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { FileService } from './file.service';
 
 describe('FileService', () => {
   let service: FileService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy },
+      ],
+    });
     service = TestBed.inject(FileService);
   });
 
